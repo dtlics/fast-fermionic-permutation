ebc222f44112082ae2267fef6a1507a7c4e47b21
Marcus Edwards <marcusstephenedwards@gmail.com>
2026-04-22 09:56:40 -0400

    Greedy scheduler

---
+++ common/scheduler.py
@@ -0,0 +1,51 @@
+from functools import reduce
+
+import pennylane as qp
+
+
+def greedy_schedule(circuit):
+    dag = qp.commutation_dag(circuit)
+    nodes = dag.get_nodes()
+
+    time = 0
+    moments = []
+    scheduled_ids = []
+
+    while len(scheduled_ids) < len(nodes):
+        moments.append([])
+        moment_ids = []
+
+        # check if we can schedule each node
+        for i, node in nodes:
+            # check if we already scheduled this node
+            if node.node_id in scheduled_ids:
+                continue
+
+            # check that there is no predecessor that has not been scheduled already (before this moment)
+            dependent = False
+            for predecessor_id in node.predecessors:
+                if predecessor_id not in scheduled_ids:
+                    dependent = True
+            if dependent:
+                continue
+
+            # check that the wires are available in the current moment
+            moment_wires = []
+            if moments[time]:
+                moment_wires += reduce(lambda acc, next: acc + next, [n.wires for n in moments[time]])
+
+            found = False
+            for wire in node.wires:
+                if wire in moment_wires:
+                    found = True
+            if found:
+                continue  # we can't schedule it do to a wire collision
+
+            # schedule the op in the current time slice
+            moments[time].append(node.op)
+            moment_ids.append(node.node_id)
+
+        time += 1
+        scheduled_ids += moment_ids
+
+    return moments
---
+++ common/tests/test_scheduler.py
@@ -0,0 +1,29 @@
+import pytest
+
+import pennylane as qp
+
+from common.scheduler import greedy_schedule
+
+
+@pytest.mark.parametrize("circuit, expected", [
+    (
+        qp.tape.qscript.QuantumScript(
+            [
+                qp.CNOT([0, 1]),
+                qp.H(0),
+                qp.X(2),
+                qp.CNOT([1, 2]),
+                qp.H(0),
+                qp.CNOT([2, 3]),
+                qp.X(1),
+                qp.CNOT([1, 2])
+            ]
+        ),
+        [[qp.CNOT(wires=[0, 1]), qp.X(2)], [qp.H(0), qp.CNOT(wires=[1, 2])], [qp.H(0), qp.CNOT(wires=[2, 3]), qp.X(1)], [qp.CNOT(wires=[1, 2])]]
+    ),
+])
+def test_gamma_primitive_depth(circuit, expected):
+    """Verify that the scheduler works as expected."""
+
+    moments = greedy_schedule(circuit)
+    assert moments == expected
